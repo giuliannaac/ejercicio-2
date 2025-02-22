7b170e1b2c5c9e33e734c30dab8cac2f35b316fc
giuliannaac <giulianna.md1@gmail.com>
2025-02-21 20:32:01 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,10 +1,22 @@
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-!pip install gapminder
-from gapminder import gapminder
-penguins = sns.load_dataset('penguins')
-penguins
-px.box(penguins,x="species",y="flipper_length_mm",color="species")
-px.box(penguins,x="sex",y="flipper_length_mm",color="sex")
+
+# Título de la aplicación
+st.title("Visualización de datos de Pingüinos")
+
+# Cargar dataset
+penguins = sns.load_dataset("penguins")
+
+# Mostrar dataset
+st.write("## Datos de los pingüinos 🐧")
+st.dataframe(penguins)
+
+# Gráfico de caja por especie
+fig1 = px.box(penguins, x="species", y="flipper_length_mm", color="species", title="Longitud de Aletas por Especie")
+st.plotly_chart(fig1)
+
+# Gráfico de caja por sexo
+fig2 = px.box(penguins, x="sex", y="flipper_length_mm", color="sex", title="Longitud de Aletas por Sexo")
+st.plotly_chart(fig2)
