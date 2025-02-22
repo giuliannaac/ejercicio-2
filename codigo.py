e67073d8572b7c6ae57c571dbc9d8b11ba105dce
giuliannaac <giulianna.md1@gmail.com>
2025-02-21 20:28:11 -0500

    Update and rename codigo to codigo.py

--- codigo.py
+++ codigo.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+!pip install gapminder
+from gapminder import gapminder
 penguins = sns.load_dataset('penguins')
 penguins
 px.box(penguins,x="species",y="flipper_length_mm",color="species")
